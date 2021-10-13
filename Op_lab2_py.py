4555e4bfca0fd20d88c38582509ec19052b8184d
KyryloLazov <90173802+KyryloLazov@users.noreply.github.com>
2021-10-13 18:36:22 +0300

    Add files via upload

---
+++ Op_lab2_py.py
@@ -0,0 +1,10 @@
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+b1 = int(input())
+b2 = int(input())
+b3 = int(input())
+if(a1/b1==a2/b2 and a1/b1==a3/b3 and a2/b2==a3/b3 ):
+    print("Вектори колінеарні")
+else:
+    print("Вектори не колінеарні")
