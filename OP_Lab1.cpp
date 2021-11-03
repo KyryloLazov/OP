06bc0c339aaee444972524eb568b296f37e0f261
KyryloLazov <90173802+KyryloLazov@users.noreply.github.com>
2021-11-03 20:32:31 +0200

    Add files via upload

---
+++ OP_Lab1.cpp
@@ -0,0 +1,15 @@
+// OP_Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int square;
+    square = (a*b)/2;
+    cout << square;
+}
